3de1010b772369ee778962ba65097a4946d7b2b1
Slimane Akalië <slimaneakalia@gmail.com>
2022-07-29 15:08:11 +0100

    add tests for context

--- src/lib/transport/context.ts
+++ src/lib/transport/context.ts
@@ -6,7 +6,7 @@ export interface Context extends WithId {
   getEvent(): JournifyEvent;
   isSame(other: Context): boolean;
   setFailedDelivery(failedDelivery: ContextFailedDelivery);
-  getFailedDelivery(): ContextFailedDelivery;
+  getFailedDelivery(): ContextFailedDelivery | null;
 }
 
 export interface ContextFactory {
@@ -45,8 +45,8 @@ class ContextImpl implements Context {
     this.failedDelivery = failedDelivery;
   }
 
-  public getFailedDelivery(): ContextFailedDelivery {
-    return this.failedDelivery;
+  public getFailedDelivery(): ContextFailedDelivery | null {
+    return this.failedDelivery ?? null;
   }
 }
 

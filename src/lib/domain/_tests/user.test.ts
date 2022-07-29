9f7bbcd8ffd88445f96d2b051655dbf41d40cc08
Slimane Akalië <slimaneakalia@gmail.com>
2022-07-29 10:34:42 +0100

    add stores update after identify call

--- src/lib/domain/_tests/user.test.ts
+++ src/lib/domain/_tests/user.test.ts
@@ -1,7 +1,9 @@
+import { v4 as uuid } from "@lukeed/uuid";
 import { UserFactoryImpl } from "../user";
 import {
   assertValueOnStores,
   createStoresForTest,
+  TestStores,
 } from "../../../test/helpers/stores";
 
 describe("UserFactoryImpl class", () => {
@@ -25,43 +27,53 @@ describe("UserFactoryImpl class", () => {
     });
 
     it("Should get the user data from the local storage when it exists", () => {
-      const stores = createStoresForTest();
-      const { local, cookies, memory } = stores;
-      const userId = "user_id_example_local_storage";
-      const anonymousId = "68edfd03-cce1-4b95-93b3-9ef7286c89ae_local";
-      const traits = {
-        email: "user@mail.com",
-        clicks: 337,
-        location: "Morocco",
-      };
-
-      local.set("journifyio_user_id", userId);
-      local.set("journifyio_anonymous_id", anonymousId);
-      local.set("journifyio_user_traits", traits);
-
-      const factory = new UserFactoryImpl(local, cookies, memory);
-      const user = factory.newUser();
-      expect(user.getUserId()).toEqual(userId);
-      expect(user.getAnonymousId()).toEqual(anonymousId);
-      expect(user.getTraits()).toEqual(traits);
-
-      assertValueOnStores(stores, "journifyio_user_id", userId);
-      assertValueOnStores(stores, "journifyio_anonymous_id", anonymousId);
-      assertValueOnStores(stores, "journifyio_user_traits", traits);
+      testFetchUserDataFromStore("local");
     });
 
-    /* it("Should get the user data from the cookies when it exists", () => {});
-     it("Should get the user data from the memory store when it exists", () => {});*/
+    it("Should get the user data from the cookies when it exists", () => {
+      testFetchUserDataFromStore("cookies");
+    });
+
+    it("Should get the user data from the memory store when it exists", () => {
+      testFetchUserDataFromStore("memory");
+    });
   });
 });
 
-/*describe("User interface", () => {
+describe("User interface", () => {
   describe("identify method", () => {
-    it("Should update the user id and traits on the stores", () => {});
-    it("Should merge the traits with the previous ones from the stores", () => {});
+    it("Should update the user id and traits on the stores", () => {
+      const stores = createStoresForTest();
+      const { local, cookies, memory } = stores;
+
+      const oldUserId = "1567893";
+      const oldTraits = { email: "user-1@gmail.com" };
+      local.set("journifyio_user_id", oldUserId);
+      cookies.set("journifyio_user_traits", oldTraits);
+
+      const factory = new UserFactoryImpl(local, cookies, memory);
+      const user = factory.newUser();
+      const newUserId = "138738937";
+      const newTraits = {
+        email: "user@mail.com",
+        clicks: 337,
+        location: "Morocco",
+        unique: uuid(),
+      };
+
+      expect(newUserId).not.toEqual(oldUserId);
+      expect(newTraits).not.toEqual(oldTraits);
+
+      user.identify(newUserId, newTraits);
+
+      assertValueOnStores(stores, "journifyio_user_id", newUserId);
+      assertValueOnStores(stores, "journifyio_user_traits", newTraits);
+    });
+
+    // it("Should merge the traits with the previous ones from the stores", () => {});
   });
 
-  describe("getUserId method", () => {
+  /*describe("getUserId method", () => {
     it("Should return null when there is no user id on different stores", () => {});
     it("Should return the same user id from the stores", () => {});
   });
@@ -73,5 +85,32 @@ describe("UserFactoryImpl class", () => {
   describe("getTraits method", () => {
     it("Should return an empty object when there is no traits on different stores", () => {});
     it("Should return the same traits from the stores", () => {});
-  });
-});*/
+  });*/
+});
+
+function testFetchUserDataFromStore(storeKey: keyof TestStores) {
+  const stores = createStoresForTest();
+  const { local, cookies, memory } = stores;
+  const userId = "user_id_example_store";
+  const anonymousId = uuid();
+  const traits = {
+    email: "user@mail.com",
+    clicks: 337,
+    location: "Morocco",
+    unique: uuid(),
+  };
+
+  stores[storeKey].set("journifyio_user_id", userId);
+  stores[storeKey].set("journifyio_anonymous_id", anonymousId);
+  stores[storeKey].set("journifyio_user_traits", traits);
+
+  const factory = new UserFactoryImpl(local, cookies, memory);
+  const user = factory.newUser();
+  expect(user.getUserId()).toEqual(userId);
+  expect(user.getAnonymousId()).toEqual(anonymousId);
+  expect(user.getTraits()).toEqual(traits);
+
+  assertValueOnStores(stores, "journifyio_user_id", userId);
+  assertValueOnStores(stores, "journifyio_anonymous_id", anonymousId);
+  assertValueOnStores(stores, "journifyio_user_traits", traits);
+}

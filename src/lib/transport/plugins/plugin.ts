21319f4a006b70c6f7238642eb786808a80f087b
Slimane Akalië <slimaneakalia@gmail.com>
2022-08-01 16:48:56 +0100

    add identify to plugins

--- src/lib/transport/plugins/plugin.ts
+++ src/lib/transport/plugins/plugin.ts
@@ -1,5 +1,6 @@
 import { Context } from "../context";
 
 export interface JPlugin {
-  identify?: (ctx: Context) => Promise<Context> | Context;
+  identify: (ctx: Context) => Promise<Context> | Context;
+  track: (ctx: Context) => Promise<Context> | Context;
 }

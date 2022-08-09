d2c3073a4b085bd3c18230a4d3d46ce5b615cc4d
Slimane Akalië <slimaneakalia@gmail.com>
2022-08-09 22:37:09 +0100

    [PATCH] Simple test for git hooks

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
-import { AnalyticsSettings } from "./api/analytics";
 import { load } from "./api/loader";
+import { AnalyticsSettings } from "./api/analytics";
 
 export { AnalyticsSettings, load };

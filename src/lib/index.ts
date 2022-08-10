6af3fbc2879b860c6eba49d0dab06767146002c2
Slimane Akalië <slimaneakalia@gmail.com>
2022-08-10 17:06:32 +0100

    add test log

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 import { load } from "./api/loader";
 import { AnalyticsSettings } from "./api/analytics";
+import { LIB_VERSION } from "./generated/libVersion";
 
-console.log("Hello from Journify");
+console.log("Hello from Journify version ", LIB_VERSION);
 
 export { AnalyticsSettings, load };

eacbe248d17a9d251fdb2cd100f4f5923a50b209
Slimane Akalië <slimaneakalia@gmail.com>
2022-08-10 22:38:41 +0100

    initial commit

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,3 @@
 import { load } from "./api/loader";
 import { AnalyticsSettings } from "./api/analytics";
-import { LIB_VERSION } from "./generated/libVersion";
-
-console.log("Hello from Journify version ", LIB_VERSION);
-
 export { AnalyticsSettings, load };

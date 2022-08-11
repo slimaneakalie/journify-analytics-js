2064164ce590bc6f9ffd9452dc953de0f7ab7248
Slimane Akalië <slimaneakalia@gmail.com>
2022-08-11 15:25:38 +0100

    add singleton load

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,7 @@
-import { load } from "./api/loader";
-import { AnalyticsSettings } from "./api/analytics";
+import { Loader } from "./api/loader";
+import { Analytics, AnalyticsSettings } from "./api/analytics";
+
+const loader: Loader = new Loader();
+const load = (settings: AnalyticsSettings): Analytics => loader.load(settings);
+
 export { AnalyticsSettings, load };
--- src/lib/transport/plugins/plugin.ts
+++ src/lib/transport/plugins/plugin.ts
@@ -1,8 +1,10 @@
 import { Context } from "../context";
+import { AnalyticsSettings } from "../../api/analytics";
 
 export interface JPlugin {
   identify: (ctx: Context) => Promise<Context> | Context;
   track: (ctx: Context) => Promise<Context> | Context;
   page: (ctx: Context) => Promise<Context> | Context;
   group: (ctx: Context) => Promise<Context> | Context;
+  updateSettings(settings: AnalyticsSettings): void;
 }

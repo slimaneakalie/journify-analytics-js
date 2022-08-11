32f1106cbdc809238e70a0d6d6e90de2739f6d6e
Slimane Akalië <slimaneakalia@gmail.com>
2022-08-11 15:42:32 +0100

    fix the index page

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,41 @@
 import { Loader } from "./api/loader";
 import { Analytics, AnalyticsSettings } from "./api/analytics";
+import { Traits } from "./domain/traits";
+import { Context } from "./transport/context";
 
 const loader: Loader = new Loader();
-const load = (settings: AnalyticsSettings): Analytics => loader.load(settings);
+let analytics: Analytics = null;
 
-export { AnalyticsSettings, load };
+function load(settings: AnalyticsSettings) {
+  analytics = loader.load(settings);
+}
+
+async function identify(userId: string, traits?: Traits): Promise<Context> {
+  checkForLoad();
+  return analytics.identify(userId, traits);
+}
+
+async function track(eventName: string, properties?: object): Promise<Context> {
+  checkForLoad();
+  return analytics.track(eventName, properties);
+}
+
+async function page(pageName?: string, properties?: object): Promise<Context> {
+  checkForLoad();
+  return analytics.page(pageName, properties);
+}
+
+async function group(groupId: string, traits?: Traits): Promise<Context> {
+  checkForLoad();
+  return analytics.group(groupId, traits);
+}
+
+function checkForLoad() {
+  if (!analytics) {
+    console.log(
+      "Analytics is not loaded, you should call the `load` function before attempting to call any other helper"
+    );
+  }
+}
+
+export { load, identify, track, page, group, AnalyticsSettings };
